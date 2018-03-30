03a096db7bdced04c3675d83e073d7d93dfee6bf
Behzod Saidov <behzod.saidov@daicompanies.com>
2018-03-29 20:00:58 -0700

    First commit

---
+++ unit-tested-plugin-demo.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name:     Unit Tested Plugin Demo
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     unit-tested-plugin-demo
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Unit_Tested_Plugin_Demo
+ */
+
+// Your code starts here.
